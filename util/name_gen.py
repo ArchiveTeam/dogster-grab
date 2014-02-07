b7049010f4b648e98c3c92437a373ff540d73817
Christopher Foo <chris.foo@gmail.com>
2014-02-06 20:03:02 -0500

    Adds name_gen.py util script.

---
+++ util/name_gen.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+
+def main():
+    start = 1  # change me
+    end = 100  # change me
+
+    for num in xrange(start, end + 1):
+        print('profile:' + str(num))
+
+
+if __name__ == '__main__':
+    main()
